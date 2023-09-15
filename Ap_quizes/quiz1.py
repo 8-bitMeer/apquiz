c2596dbfa00f0912a584b30e7fab69cc03209a39
8-bitMeer <144816003+8-bitMeer@users.noreply.github.com>
2023-09-15 14:40:27 +0000

    Ap_quiz

--- Ap_quizes/quiz1.py
+++ Ap_quizes/quiz1.py
@@ -1,36 +1,37 @@
 # 1. In your on words, describe what a variable is.
-
+A variable is a value that can be changed depending on the conditions.
 # 2. Name three (3) operators we've learned about in class and describe 
 # a situation where you would use each one. 
-
+Addition:adds two values together, Subtraction:subtracts the second value from the first, Multiplication:multiplies two values together
 # 3. Describe the difference between camal case and pascal case.
-
+The only difference between the two is that pascal case requires the first letter of the first word to also be capitalized.
 # 4. Which operator is being used in the example below
 numberOfApples = 10
 numberOfOranges = 12
-
+Less Than
 numberOfApples < numberOfOranges
 
 # 5. You have been given a $100.00 dollar gift card from a family member. Another 
 # family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
-
+$14.50
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
-
+43243
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
 # They want to know a visitor's full name, their age, if they have an allergy, and what their 
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
-
+enter the codespace
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
 # If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
 # have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
-
+allergy:non-allergy
 # 9. In your own words, describe what a program is?
-
+A program is a set of instructions that a computer follows in order to perform a particular task.
 # 10. Describe the difference between a float and an integer. 
+An integer is a number without a decimal point. A float is a floating-point number, which means it is a number that has a decimal place.
