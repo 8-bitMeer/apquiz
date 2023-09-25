e30189f736707e01eb1ea48d20642284268c5268
8-bitMeer <144816003+8-bitMeer@users.noreply.github.com>
2023-09-25 14:57:56 +0000

    functions

--- ap_python_activities/functionstarteractivity.py
+++ ap_python_activities/functionstarteractivity.py
@@ -1,11 +1,13 @@
 # 1. In your own words, describe what a function is
-
+function is a container for code having has 1 objective
+"reusable code instructions"
 # 2. What is are function parameters and arguments and describe
 # the difference between the 2. 
-
+function paramaters are names listed in the function definition.
+Function arguments are the real values passed to (and recieved by) the function.
 # 3. write a function that will print out a welcome message
 # that includes a users name. You will need to use parameters and arguments
-
+print("Hello, 8-bitMeer")
 # 4. Write a function that will do a calculation that takes 3 parameters.
 # Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
 
