a3ce9a0b853ffd60f70b78b81276ee0136ad90bf
8-bitMeer <144816003+8-bitMeer@users.noreply.github.com>
2023-09-28 15:02:37 +0000

    classwork

--- ap_python_activities/conditionalactivity.py
+++ ap_python_activities/conditionalactivity.py
@@ -1,11 +1,29 @@
 # 1. What is the difference between 
 # a parameter and an argument in a python function
-A parameter is the variable listed inside the parentheses in the function definition. An argument is the value that are sent to the function when it is called.
+"parameter are placeholders in our function definition."
+"We are expecting some data."
+"An agrument is value or data that is sent to the function it's called."
+
 # 2. In your own words, describe what 
 # a conditional statement (if/else) is 
-A conditional statement describes the relationship between two events based on certain conditions.
+" a tool that allows you make"
+"decesions depending on if a certain condition is met." 
+
 # 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
-forty-three is greater than twenty-four.
+a= 200
+b= 2
+
+if b > a:
+    print('B is greater than A')
+else:
+    print('B is not greater than A')
+
+#name = input('Enter your name')
+
+# if name =='Ian': # not the same ian- case sensative 
+#   print('this is the correct name')
+# else:
+#   print("not my name")
 # 4. Write a conditional statement for food in the refridgerator.
 # your conditional statement should be wrapped in a function that takes one (1)
 # parameter. The data type for this parameter should be true or false. 
@@ -14,9 +32,22 @@ forty-three is greater than twenty-four.
 # NOT food in the fridge, print time to shop. 
 # When you call your function there should be an argument that accepts 
 # a boolean. 
-The drinks are greater than the food items. print("Time to shop")
+def foodInFridge(food):
+    if food == True:
+        print('Time to cook.')
+    else:
+        print('Time to shop.')
+#foodInFridge(True)
 # 5. Create a function that checks the inventory of cereal for a store. 
 # your function should parameter should accept an integer. In your function
 # use a conditional statement to determine if you need to order more cereal.
 # If there is more than 10 boxes, print "inventory full", else if there are less than 
 # 10 boxes print "we need to order more cereal".
+def check_cereal_inventory(quantity):
+    if quantity > 10:
+        print('inventory is full.')
+    else:
+        print('order more cereal.')
+
+print('___________________________')
+check_cereal_inventory(8)
