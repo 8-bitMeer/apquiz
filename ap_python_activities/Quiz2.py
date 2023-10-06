31b0978d68100ee8f02ada1beeb3606f5cf205fc
8-bitMeer <144816003+8-bitMeer@users.noreply.github.com>
2023-10-06 11:18:57 -0400

    Update Quiz2.py

--- ap_python_activities/Quiz2.py
+++ ap_python_activities/Quiz2.py
@@ -1,6 +1,6 @@
 # 1. Create a function that will multiply two (2) values that are passed into the function as arguments. 
 # Your function should print out the result of the two numbers that have been multiplied.
-from numpy import append
+
 
 
 def Multiply(num1, num2):
@@ -12,9 +12,8 @@ print(Multiply(4,9))
 # two arguments and then the sum of the first two (2) should be divided by the third argument. 
 # You function should then print the result. 
 # This function adds two numbers
-
-#call function with 3 functions
-(40,80,30)
+call function with 3 functions
+numbers = (40,80,30)
 print(4)
 # 3. Create a elevator function that will run specific lines of code based on the conditions provided by a user. If the user types in 101,
 # the function should print out they are going to the boys latin office, if they type in 203, they are going to the gym, 
@@ -22,21 +21,22 @@ print(4)
 # enter a valid floor number.
 
 # hint you will need to look into using conditional statements
-
+print(please insert valid floor number)
 # 4. Write a simple conditional statement that uses a boolean that will print if it is daytime or nighttime.
-
+boolean : print('daytime') = True
 # 5. What function would you use if you wanted to add and element/ value to a list data type? Explain why you would use it.
 append
 # 6. Do some research and find the correct built-in python function that will reverse the order of the following list.
 # then print your list in the reverse order.
 
 random_number_list = [0,1,2,3,4,5,6,7,8]
-
+slice technique
+output: [8,7,6,5,4,3,2,1,0]
 # 7.Do some research and find the correcrt built-in python function that will allow you to find the largest number in the following list.
 # then print the largest number
 ranom_number_list2 = [100,230,40,39403,19]
 list1 = [100,230,40,39403,19]
-
+output : 39403
 # 8. A security company has hired you as an engineer to help them develop a program that will only let users into the building 
 # if they enter a specific password. They given you the following information to use to build this program.
 # - they want users to be able to enter a series of codes to get access
@@ -44,12 +44,13 @@ list1 = [100,230,40,39403,19]
 # - if they get this correct, they then want them to enter another value, which is 3039
 # - if this is correct they will get access to the building
 # - if they have the wrong answer in either scenario they will get a message saying access denied. 
-
+print('access denied')
 # 9. What does it mean to call a function? Why do we call functions. 
-# you can use the variable below to enter you ansewer. 
-answer9='your answer here'
+# you can use the variable below to enter you answer. 
+answer9='to run or execute the set of actions'
 
 # 10. Find and print each value at the specific indexes provided in the list.
 # find and print the values/words at index 3, 4, and 6 
 
 shopping_cart = ['apples','water','chicken','ice cream','ground beef','string beans','oranges']
+output : ['ice cream','ground beef','oranges']
