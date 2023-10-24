7081b5db1fd70b608347199812fda08afb3106b2
8-bitMeer <144816003+8-bitMeer@users.noreply.github.com>
2023-10-24 11:23:09 -0400

    forloopswarmup.py

---
+++ ap_python_activities/forloopswarmup.py
@@ -0,0 +1,10 @@
+# 1. In your own words, describe what a for loop is?
+a for loop is a loop that's used for iterating over a sequence
+# 2. What is the difference between a FOR loop and a WHILE loop?
+# Provide two (2) examples of each. 
+for loops can iterate over a sequence.
+while loops: the statement being executed must be true.
+# 3. Create a FOR loop that will go through a list of names 
+# and print all the names that start with the letter "R".
+['Rebcca', 'Robert', 'Rose']
+names=['Michael','Rebecca','William','Kareem','Robert','Rose','Jason']
