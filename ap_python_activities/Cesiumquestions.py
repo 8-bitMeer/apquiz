cc3db9c31c957244130acccda0ed230809b22400
8-bitMeer <144816003+8-bitMeer@users.noreply.github.com>
2023-10-27 11:12:39 -0400

    Cesiumquestions.py

---
+++ ap_python_activities/Cesiumquestions.py
@@ -0,0 +1,5 @@
+Question 1: What Process do you use when concocting lines of code?
+
+Question 2: Are there certains mishaps when you try testing the code that you make?
+
+Question 3: Are you planning to revolutionize your technology or your methods to increase your effiency?
